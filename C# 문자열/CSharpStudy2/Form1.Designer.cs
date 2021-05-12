bacd3fe91b266ba354b000dd9033d3885fc06c1b
CShrimp42634 <CShrimp42634@gmail.com>
2021-05-12 18:24:39 +0900

    C# 문자열 추가

---
+++ C# 문자열/CSharpStudy2/Form1.Designer.cs
@@ -0,0 +1,314 @@
+﻿namespace CSharpStudy2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblText = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.lblContain = new System.Windows.Forms.Label();
+            this.lblEquals = new System.Windows.Forms.Label();
+            this.lblLength = new System.Windows.Forms.Label();
+            this.lblReplace = new System.Windows.Forms.Label();
+            this.lblSplit = new System.Windows.Forms.Label();
+            this.lblSubstring = new System.Windows.Forms.Label();
+            this.lblTolower = new System.Windows.Forms.Label();
+            this.lblToUpper = new System.Windows.Forms.Label();
+            this.lblTrim = new System.Windows.Forms.Label();
+            this.lblSplit3 = new System.Windows.Forms.Label();
+            this.lblSplit2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblText
+            // 
+            this.lblText.AutoSize = true;
+            this.lblText.Location = new System.Drawing.Point(39, 23);
+            this.lblText.Name = "lblText";
+            this.lblText.Size = new System.Drawing.Size(106, 12);
+            this.lblText.TabIndex = 0;
+            this.lblText.Text = "Sample,Test,Text";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(235, 18);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "실행";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(39, 86);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(48, 12);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Contain";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(40, 120);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(44, 12);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Equals";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(39, 155);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(43, 12);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Length";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(39, 189);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(51, 12);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "Replace";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(39, 219);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(29, 12);
+            this.label6.TabIndex = 6;
+            this.label6.Text = "Split";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(257, 86);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(58, 12);
+            this.label7.TabIndex = 7;
+            this.label7.Text = "Substring";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(257, 120);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(55, 12);
+            this.label8.TabIndex = 8;
+            this.label8.Text = "ToLower";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(257, 155);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(55, 12);
+            this.label9.TabIndex = 9;
+            this.label9.Text = "ToUPPer";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(255, 189);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(31, 12);
+            this.label10.TabIndex = 10;
+            this.label10.Text = "Trim";
+            // 
+            // lblContain
+            // 
+            this.lblContain.AutoSize = true;
+            this.lblContain.Location = new System.Drawing.Point(128, 86);
+            this.lblContain.Name = "lblContain";
+            this.lblContain.Size = new System.Drawing.Size(11, 12);
+            this.lblContain.TabIndex = 11;
+            this.lblContain.Text = "-";
+            // 
+            // lblEquals
+            // 
+            this.lblEquals.AutoSize = true;
+            this.lblEquals.Location = new System.Drawing.Point(128, 120);
+            this.lblEquals.Name = "lblEquals";
+            this.lblEquals.Size = new System.Drawing.Size(11, 12);
+            this.lblEquals.TabIndex = 12;
+            this.lblEquals.Text = "-";
+            // 
+            // lblLength
+            // 
+            this.lblLength.AutoSize = true;
+            this.lblLength.Location = new System.Drawing.Point(128, 155);
+            this.lblLength.Name = "lblLength";
+            this.lblLength.Size = new System.Drawing.Size(11, 12);
+            this.lblLength.TabIndex = 13;
+            this.lblLength.Text = "-";
+            // 
+            // lblReplace
+            // 
+            this.lblReplace.AutoSize = true;
+            this.lblReplace.Location = new System.Drawing.Point(128, 189);
+            this.lblReplace.Name = "lblReplace";
+            this.lblReplace.Size = new System.Drawing.Size(11, 12);
+            this.lblReplace.TabIndex = 14;
+            this.lblReplace.Text = "-";
+            // 
+            // lblSplit
+            // 
+            this.lblSplit.AutoSize = true;
+            this.lblSplit.Location = new System.Drawing.Point(128, 219);
+            this.lblSplit.Name = "lblSplit";
+            this.lblSplit.Size = new System.Drawing.Size(11, 12);
+            this.lblSplit.TabIndex = 15;
+            this.lblSplit.Text = "-";
+            // 
+            // lblSubstring
+            // 
+            this.lblSubstring.AutoSize = true;
+            this.lblSubstring.Location = new System.Drawing.Point(367, 86);
+            this.lblSubstring.Name = "lblSubstring";
+            this.lblSubstring.Size = new System.Drawing.Size(11, 12);
+            this.lblSubstring.TabIndex = 16;
+            this.lblSubstring.Text = "-";
+            // 
+            // lblTolower
+            // 
+            this.lblTolower.AutoSize = true;
+            this.lblTolower.Location = new System.Drawing.Point(367, 120);
+            this.lblTolower.Name = "lblTolower";
+            this.lblTolower.Size = new System.Drawing.Size(11, 12);
+            this.lblTolower.TabIndex = 17;
+            this.lblTolower.Text = "-";
+            // 
+            // lblToUpper
+            // 
+            this.lblToUpper.AutoSize = true;
+            this.lblToUpper.Location = new System.Drawing.Point(367, 155);
+            this.lblToUpper.Name = "lblToUpper";
+            this.lblToUpper.Size = new System.Drawing.Size(11, 12);
+            this.lblToUpper.TabIndex = 18;
+            this.lblToUpper.Text = "-";
+            // 
+            // lblTrim
+            // 
+            this.lblTrim.AutoSize = true;
+            this.lblTrim.Location = new System.Drawing.Point(367, 189);
+            this.lblTrim.Name = "lblTrim";
+            this.lblTrim.Size = new System.Drawing.Size(11, 12);
+            this.lblTrim.TabIndex = 19;
+            this.lblTrim.Text = "-";
+            // 
+            // lblSplit3
+            // 
+            this.lblSplit3.AutoSize = true;
+            this.lblSplit3.Location = new System.Drawing.Point(128, 288);
+            this.lblSplit3.Name = "lblSplit3";
+            this.lblSplit3.Size = new System.Drawing.Size(11, 12);
+            this.lblSplit3.TabIndex = 20;
+            this.lblSplit3.Text = "-";
+            // 
+            // lblSplit2
+            // 
+            this.lblSplit2.AutoSize = true;
+            this.lblSplit2.Location = new System.Drawing.Point(128, 256);
+            this.lblSplit2.Name = "lblSplit2";
+            this.lblSplit2.Size = new System.Drawing.Size(11, 12);
+            this.lblSplit2.TabIndex = 21;
+            this.lblSplit2.Text = "-";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(476, 348);
+            this.Controls.Add(this.lblSplit2);
+            this.Controls.Add(this.lblSplit3);
+            this.Controls.Add(this.lblTrim);
+            this.Controls.Add(this.lblToUpper);
+            this.Controls.Add(this.lblTolower);
+            this.Controls.Add(this.lblSubstring);
+            this.Controls.Add(this.lblSplit);
+            this.Controls.Add(this.lblReplace);
+            this.Controls.Add(this.lblLength);
+            this.Controls.Add(this.lblEquals);
+            this.Controls.Add(this.lblContain);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.lblText);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblText;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label lblContain;
+        private System.Windows.Forms.Label lblEquals;
+        private System.Windows.Forms.Label lblLength;
+        private System.Windows.Forms.Label lblReplace;
+        private System.Windows.Forms.Label lblSplit;
+        private System.Windows.Forms.Label lblSubstring;
+        private System.Windows.Forms.Label lblTolower;
+        private System.Windows.Forms.Label lblToUpper;
+        private System.Windows.Forms.Label lblTrim;
+        private System.Windows.Forms.Label lblSplit3;
+        private System.Windows.Forms.Label lblSplit2;
+    }
+}
+
---
+++ C# 문자열/CSharpStudy2/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CSharpStudy2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //Sample,Test,Text
+            string strText = lblText.Text;
+
+            lblContain.Text = strText.Contains("Text").ToString();
+            lblEquals.Text = strText.Equals("Test").ToString();
+            lblLength.Text = strText.Length.ToString();
+            lblReplace.Text = strText.Replace("Test", "l can").ToString();
+
+            string[] strSplit = strText.Split(',');
+            lblSplit.Text = strSplit[0].ToString();  //"Sample"
+            lblSplit2.Text = strSplit[1].ToString(); //"Test"
+            lblSplit3.Text = strSplit[2].ToString(); //"Text"
+
+            lblSubstring.Text = strText.Substring(3, 5).ToString();
+            lblTolower.Text = strText.ToLower().ToString();
+            lblToUpper.Text = strText.ToUpper().ToString();
+            lblTrim.Text = strText.Trim().ToString();
+        }
+    }
+}
