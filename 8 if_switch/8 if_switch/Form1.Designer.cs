057abf87757010a496f54165cffb7b618cfddf96
CShrimp42634 <CShrimp42634@gmail.com>
2021-05-31 10:52:16 +0900

    추가

--- 8 if_switch/8 if_switch/Form1.Designer.cs
+++ 8 if_switch/8 if_switch/Form1.Designer.cs
@@ -30,6 +30,16 @@
         {
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
+            this.nNumber1 = new System.Windows.Forms.NumericUpDown();
+            this.nNumber2 = new System.Windows.Forms.NumericUpDown();
+            this.btnifResult = new System.Windows.Forms.Button();
+            this.lblifResult = new System.Windows.Forms.Label();
+            this.cboxDay = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lblswitchResult = new System.Windows.Forms.Label();
+            this.btnswitchResult = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.nNumber1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nNumber2)).BeginInit();
             this.SuspendLayout();
             // 
             // label1
@@ -50,15 +60,103 @@
             this.label2.TabIndex = 1;
             this.label2.Text = "Number2)";
             // 
+            // nNumber1
+            // 
+            this.nNumber1.Location = new System.Drawing.Point(12, 24);
+            this.nNumber1.Name = "nNumber1";
+            this.nNumber1.Size = new System.Drawing.Size(61, 21);
+            this.nNumber1.TabIndex = 2;
+            // 
+            // nNumber2
+            // 
+            this.nNumber2.Location = new System.Drawing.Point(103, 24);
+            this.nNumber2.Name = "nNumber2";
+            this.nNumber2.Size = new System.Drawing.Size(61, 21);
+            this.nNumber2.TabIndex = 3;
+            // 
+            // btnifResult
+            // 
+            this.btnifResult.Location = new System.Drawing.Point(223, 12);
+            this.btnifResult.Name = "btnifResult";
+            this.btnifResult.Size = new System.Drawing.Size(103, 23);
+            this.btnifResult.TabIndex = 4;
+            this.btnifResult.Text = "Number 비교";
+            this.btnifResult.UseVisualStyleBackColor = true;
+            this.btnifResult.Click += new System.EventHandler(this.btnifResult_Click);
+            // 
+            // lblifResult
+            // 
+            this.lblifResult.AutoSize = true;
+            this.lblifResult.Location = new System.Drawing.Point(12, 81);
+            this.lblifResult.Name = "lblifResult";
+            this.lblifResult.Size = new System.Drawing.Size(11, 12);
+            this.lblifResult.TabIndex = 5;
+            this.lblifResult.Text = "-";
+            // 
+            // cboxDay
+            // 
+            this.cboxDay.FormattingEnabled = true;
+            this.cboxDay.Items.AddRange(new object[] {
+            "월",
+            "화",
+            "수",
+            "목",
+            "금",
+            "토",
+            "일"});
+            this.cboxDay.Location = new System.Drawing.Point(12, 184);
+            this.cboxDay.Name = "cboxDay";
+            this.cboxDay.Size = new System.Drawing.Size(70, 20);
+            this.cboxDay.TabIndex = 6;
+            this.cboxDay.Text = "월";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(10, 169);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(72, 12);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "요일 (선택))";
+            // 
+            // lblswitchResult
+            // 
+            this.lblswitchResult.AutoSize = true;
+            this.lblswitchResult.Location = new System.Drawing.Point(12, 228);
+            this.lblswitchResult.Name = "lblswitchResult";
+            this.lblswitchResult.Size = new System.Drawing.Size(11, 12);
+            this.lblswitchResult.TabIndex = 8;
+            this.lblswitchResult.Text = "-";
+            // 
+            // btnswitchResult
+            // 
+            this.btnswitchResult.Location = new System.Drawing.Point(221, 184);
+            this.btnswitchResult.Name = "btnswitchResult";
+            this.btnswitchResult.Size = new System.Drawing.Size(103, 23);
+            this.btnswitchResult.TabIndex = 9;
+            this.btnswitchResult.Text = "선택요일확인";
+            this.btnswitchResult.UseVisualStyleBackColor = true;
+            this.btnswitchResult.Click += new System.EventHandler(this.btnswitchResult_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(510, 422);
+            this.Controls.Add(this.btnswitchResult);
+            this.Controls.Add(this.lblswitchResult);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.cboxDay);
+            this.Controls.Add(this.lblifResult);
+            this.Controls.Add(this.btnifResult);
+            this.Controls.Add(this.nNumber2);
+            this.Controls.Add(this.nNumber1);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "월";
+            ((System.ComponentModel.ISupportInitialize)(this.nNumber1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nNumber2)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -68,6 +166,14 @@
 
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.NumericUpDown nNumber1;
+        private System.Windows.Forms.NumericUpDown nNumber2;
+        private System.Windows.Forms.Button btnifResult;
+        private System.Windows.Forms.Label lblifResult;
+        private System.Windows.Forms.ComboBox cboxDay;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lblswitchResult;
+        private System.Windows.Forms.Button btnswitchResult;
     }
 }
 
