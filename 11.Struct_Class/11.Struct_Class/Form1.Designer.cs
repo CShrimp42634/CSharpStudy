6962ffab6817b794338a2986e86e7dba2c5a75a0
CShrimp42634 <CShrimp42634@gmail.com>
2021-06-09 10:20:22 +0900

    추가

---
+++ 11.Struct_Class/11.Struct_Class/Form1.Designer.cs
@@ -0,0 +1,182 @@
+﻿namespace _11.Struct_Class
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.pboxSun = new System.Windows.Forms.PictureBox();
+            this.pboxMoon = new System.Windows.Forms.PictureBox();
+            this.pboxNone = new System.Windows.Forms.PictureBox();
+            this.pboxStar = new System.Windows.Forms.PictureBox();
+            this.rdoPlayer1 = new System.Windows.Forms.RadioButton();
+            this.rdoPlayer2 = new System.Windows.Forms.RadioButton();
+            this.lboxResult1 = new System.Windows.Forms.ListBox();
+            this.lboxResult2 = new System.Windows.Forms.ListBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pboxSun)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pboxMoon)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pboxNone)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pboxStar)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(275, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "-Player는 한번 씩 돌아가면서 그림을 선택 합니다";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 47);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(425, 12);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "- 각 5회 진행 후 하늘,검정,살색의 숫자의 합이 가장 높은 사람이 승리 합니다.";
+            // 
+            // pboxSun
+            // 
+            this.pboxSun.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.pboxSun.Location = new System.Drawing.Point(14, 78);
+            this.pboxSun.Name = "pboxSun";
+            this.pboxSun.Size = new System.Drawing.Size(108, 152);
+            this.pboxSun.TabIndex = 2;
+            this.pboxSun.TabStop = false;
+            // 
+            // pboxMoon
+            // 
+            this.pboxMoon.BackColor = System.Drawing.Color.Black;
+            this.pboxMoon.Location = new System.Drawing.Point(146, 78);
+            this.pboxMoon.Name = "pboxMoon";
+            this.pboxMoon.Size = new System.Drawing.Size(108, 152);
+            this.pboxMoon.TabIndex = 3;
+            this.pboxMoon.TabStop = false;
+            // 
+            // pboxNone
+            // 
+            this.pboxNone.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.pboxNone.Location = new System.Drawing.Point(411, 78);
+            this.pboxNone.Name = "pboxNone";
+            this.pboxNone.Size = new System.Drawing.Size(108, 152);
+            this.pboxNone.TabIndex = 4;
+            this.pboxNone.TabStop = false;
+            // 
+            // pboxStar
+            // 
+            this.pboxStar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.pboxStar.Location = new System.Drawing.Point(278, 78);
+            this.pboxStar.Name = "pboxStar";
+            this.pboxStar.Size = new System.Drawing.Size(108, 152);
+            this.pboxStar.TabIndex = 5;
+            this.pboxStar.TabStop = false;
+            // 
+            // rdoPlayer1
+            // 
+            this.rdoPlayer1.AutoSize = true;
+            this.rdoPlayer1.Location = new System.Drawing.Point(12, 256);
+            this.rdoPlayer1.Name = "rdoPlayer1";
+            this.rdoPlayer1.Size = new System.Drawing.Size(65, 16);
+            this.rdoPlayer1.TabIndex = 6;
+            this.rdoPlayer1.TabStop = true;
+            this.rdoPlayer1.Text = "Player1";
+            this.rdoPlayer1.UseVisualStyleBackColor = true;
+            // 
+            // rdoPlayer2
+            // 
+            this.rdoPlayer2.AutoSize = true;
+            this.rdoPlayer2.Location = new System.Drawing.Point(309, 256);
+            this.rdoPlayer2.Name = "rdoPlayer2";
+            this.rdoPlayer2.Size = new System.Drawing.Size(65, 16);
+            this.rdoPlayer2.TabIndex = 7;
+            this.rdoPlayer2.TabStop = true;
+            this.rdoPlayer2.Text = "Player2";
+            this.rdoPlayer2.UseVisualStyleBackColor = true;
+            // 
+            // lboxResult1
+            // 
+            this.lboxResult1.FormattingEnabled = true;
+            this.lboxResult1.ItemHeight = 12;
+            this.lboxResult1.Location = new System.Drawing.Point(12, 278);
+            this.lboxResult1.Name = "lboxResult1";
+            this.lboxResult1.Size = new System.Drawing.Size(258, 88);
+            this.lboxResult1.TabIndex = 8;
+            this.lboxResult1.UseTabStops = false;
+            // 
+            // lboxResult2
+            // 
+            this.lboxResult2.FormattingEnabled = true;
+            this.lboxResult2.ItemHeight = 12;
+            this.lboxResult2.Location = new System.Drawing.Point(309, 278);
+            this.lboxResult2.Name = "lboxResult2";
+            this.lboxResult2.Size = new System.Drawing.Size(224, 88);
+            this.lboxResult2.TabIndex = 9;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(545, 373);
+            this.Controls.Add(this.lboxResult2);
+            this.Controls.Add(this.lboxResult1);
+            this.Controls.Add(this.rdoPlayer2);
+            this.Controls.Add(this.rdoPlayer1);
+            this.Controls.Add(this.pboxStar);
+            this.Controls.Add(this.pboxNone);
+            this.Controls.Add(this.pboxMoon);
+            this.Controls.Add(this.pboxSun);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pboxSun)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pboxMoon)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pboxNone)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pboxStar)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.PictureBox pboxSun;
+        private System.Windows.Forms.PictureBox pboxMoon;
+        private System.Windows.Forms.PictureBox pboxNone;
+        private System.Windows.Forms.PictureBox pboxStar;
+        private System.Windows.Forms.RadioButton rdoPlayer1;
+        private System.Windows.Forms.RadioButton rdoPlayer2;
+        private System.Windows.Forms.ListBox lboxResult1;
+        private System.Windows.Forms.ListBox lboxResult2;
+    }
+}
+
